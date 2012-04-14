9fef6959b40842bc8f8a39dd483a0000ef50ce92
Andrew Cutler <macropin@gmail.com>
2012-04-14 14:24:21 +1000

    Fix dep link

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@
 from setuptools import setup
 
 setup(name='dms_testing',
-    version='0.1.0',
+    version='0.1.1',
     long_description=open('README.md').read(),
     url='https://github.com/macropin/adlibre-dms-testing',
     packages=['dms_testing',],
-    install_requires=['django','lorem_pdf', 'rng'],
+    install_requires=['django','django_lorem_pdf', 'rng'],
     package_data={ 'lorem_pdf': ['templates/*',] },
     dependency_links = [
-        "https://github.com/macropin/django-lorem-pdf/tarball/master#egg=lorem_pdf",
+        "https://github.com/macropin/django-lorem-pdf/tarball/master#egg=django_lorem_pdf",
 	"https://github.com/macropin/random-name-generator/tarball/master#egg=rng",
     ],
 )
