bb6e3f76e2ba48208322fe550e81677083e210aa
Andrew Cutler <macropin@gmail.com>
2012-04-14 14:54:26 +1000

    Modify to make ready for Django project

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@
 from setuptools import setup
 
 setup(name='dms_testing',
-    version='0.1.2',
+    version='0.1.3',
     long_description=open('README.md').read(),
     url='https://github.com/macropin/adlibre-dms-testing',
-    packages=['dms_testing',],
+    packages=['apps'],
     install_requires=['django', 'lorem_pdf', 'random_name_generator'],
     dependency_links = [
         "https://github.com/macropin/django-lorem-pdf/tarball/master#egg=lorem-pdf",
