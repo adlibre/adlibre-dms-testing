7bd1bda6644cf5848a2fd388a153efd3bb30979a
Andrew Cutler <macropin@gmail.com>
2012-04-14 14:43:14 +1000

    fix deploy

--- setup.py
+++ setup.py
@@ -10,6 +10,6 @@ setup(name='dms_testing',
     install_requires=['django', 'lorem_pdf', 'random_name_generator'],
     dependency_links = [
         "https://github.com/macropin/django-lorem-pdf/tarball/master#egg=lorem-pdf",
-	"https://github.com/macropin/random-name-generator/tarball/master#egg=rng",
+	"https://github.com/macropin/random-name-generator/tarball/master#egg=random-name-generator",
     ],
 )
