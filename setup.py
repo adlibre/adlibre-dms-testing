ca647a5508148f6ca93818f665040e1b3e25258a
Andrew Cutler <macropin@gmail.com>
2012-04-20 17:14:08 +1000

    Moved project to adlibre

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 setup(name='dms_testing',
     version='0.1.5',
     long_description=open('README.md').read(),
-    url='https://github.com/macropin/adlibre-dms-testing',
+    url='https://github.com/adlibre/adlibre-dms-testing',
     packages=['dmstest'],
     scripts=['manage.py',],
     install_requires=['django==1.4', 'lorem_pdf', 'name_generator'],
